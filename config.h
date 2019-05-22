b56f47cdd2a1e1072c606d8a8c9b32fea9127e06
Toni Mägel <tmaegel@posteo.de>
2019-05-22 17:44:02 +0200

    modified colors and some key shortcuts (tagmon, focusmon)

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Tamsyn:size=10" };
 static const char dmenufont[]       = "Tamsyn:size=10";
-static const char normbordercolor[] = "#262729";
-static const char normbgcolor[]     = "#262729";
-static const char normfgcolor[]     = "#bbbbbb";
+static const char normbordercolor[] = "#262626";
+static const char normbgcolor[]     = "#262626";
+static const char normfgcolor[]     = "#aaaaaa";
 static const char selbordercolor[]  = "#7b7e85";
-static const char selbgcolor[]      = "#262729";
+static const char selbgcolor[]      = "#262626";
 static const char selfgcolor[]      = "#FFFFFF";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
@@ -90,10 +90,10 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
     { MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
-    { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
-	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { ALTKEY,                       XK_Left,   focusmon,       {.i = -1 } },
+	{ ALTKEY,                       XK_Right,  focusmon,       {.i = +1 } },
+	{ ALTKEY|ShiftMask,             XK_Left,   tagmon,         {.i = -1 } },
+	{ ALTKEY|ShiftMask,             XK_Right,  tagmon,         {.i = +1 } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
